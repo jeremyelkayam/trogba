312a6a4b44e09dc60fbb7631578c4e00c98c42a5
Jeremy Elkayam <jeremyelkayam@gmail.com>
2022-11-02 17:23:23 -0400

    background for the title screen

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <bn_keypad.h>
 #include <bn_sound_items.h>
 #include <bn_timer.h>
+#include <bn_regular_bg_ptr.h>
 #include "bn_regular_bg_items_titlebg.h"
 #include "Player.h"
 
@@ -15,16 +16,12 @@ int main()
     bn::timer kicktimer;
     bool kicked=false;
     bn::sound_items::themesong.play(1);
-    Player tragdar = Player();
+    // Player tragdar = Player();
 
-    // bn::regular_bg_ptr regular_bg = bn::regular_bg_items::titlebg.create_bg(0, 0);
+    bn::regular_bg_ptr titlebg = bn::regular_bg_items::titlebg.create_bg(-3, 48);
 
     while(true)
     {
-
-        
-        
-
         if(!kicked && bn::keypad::select_pressed()){
             BN_LOG("kick. TROGADOR");
             bn::sound_items::kick.play(1);
@@ -35,7 +32,27 @@ int main()
             bn::sound_items::trogador.play(1);
             kicked=false;
         }
-        tragdar.update();
+
+        //debug adjustment 
+        // if(bn::keypad::left_held()){
+        //     titlebg.set_x(titlebg.x() - 1);
+        //     BN_LOG("xcor ", titlebg.x());
+        // }
+        // else if(bn::keypad::right_held()){
+        //     titlebg.set_x(titlebg.x() + 1);
+        //     BN_LOG("xcor ", titlebg.x());
+        // }
+
+        // if(bn::keypad::up_held()){
+        //     titlebg.set_y(titlebg.y() - 1);
+        //     BN_LOG("ycor", titlebg.y());
+        // }
+        // else if(bn::keypad::down_held()){
+        //     titlebg.set_y(titlebg.y() + 1);
+        //     BN_LOG("ycor", titlebg.y());
+        // }
+
+        // tragdar.update();
         bn::core::update();
     }
 }
