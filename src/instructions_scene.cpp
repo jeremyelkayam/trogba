693c35c4296342b562075bda8b59092de029ad0b
Jeremy Elkayam <jeremyelkayam@gmail.com>
2022-11-03 16:00:10 -0400

    tried and failed to add text

--- src/instructions_scene.cpp
+++ src/instructions_scene.cpp
@@ -1,13 +1,22 @@
-#include "instructions_scene.h"
-#include "scene_type.h"
 #include <bn_sound_items.h>
 #include <bn_log.h>
+#include <bn_sprite_text_generator.h>
+#include <bn_sprite_ptr.h>
+#include "common_variable_8x8_sprite_font.h"
+#include "instructions_scene.h"
+#include "scene_type.h"
+
 
 namespace trog {
 
 instructions_scene::instructions_scene(){
 
     bn::sound_items::trogador.play(1);
+    bn::sprite_text_generator text_generator(common::variable_8x8_sprite_font);
+    text_generator.set_center_alignment();
+
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
+    text_generator.generate(0, 100, "press a to continute", text_sprites);
 }
 
 bn::optional<scene_type> instructions_scene::update(){
