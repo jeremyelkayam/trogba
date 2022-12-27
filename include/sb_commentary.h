68367b37fbfd14eed5178e824183abca0e45aa2e
Jeremy Elkayam <jeremyelkayam@gmail.com>
2022-12-27 15:08:36 -0500

    sb commentary class

--- include/sb_commentary.h
+++ include/sb_commentary.h
@@ -18,6 +18,14 @@ namespace trog {
             static void im_in_this_game()  {bn::sound_items::sb_iminthisgame.play(_volume);}
             static void i_sound_realistic()  {bn::sound_items::sb_isoundrealistic.play(_volume);}
             static void roast_kerrek() {bn::sound_items::sb_roastkerrek.play(_volume);}
-
+            static void gameover_arch() {bn::sound_items::sb_archer_gameover.play(_volume);}
+            static void arrowed();
+            static void sworded();
+            static void burninate();
+            static void level_win_pause();
+            static void level_win_scene();
+            static void stomp_peasant();
+            static void ignite_peasant();
+            static void ignite_cottage();
     };
 }
--- src/sb_commentary.cpp
+++ src/sb_commentary.cpp
@@ -20,5 +20,71 @@ bool sb_commentary::gameover(unsigned int score){
     return false;
 }
 
+void sb_commentary::arrowed(){
+    if(rand() % 2 == 0){
+        bn::sound_items::sb_stupid_archdeluxe.play(_volume);
+    }
+}
+
+void sb_commentary::sworded(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_stupidknight.play(_volume);
+    }else if(rand_num == 1){
+        bn::sound_items::sb_worstgame.play(_volume);
+    }
+}
+
+void sb_commentary::burninate(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_tablesturn.play(_volume);
+    }else if(rand_num == 1){
+        bn::sound_items::sb_advantage.play(_volume);
+    }
+}
+
+void sb_commentary::level_win_pause(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_ohyeah.play(_volume);
+    }
+}
+void sb_commentary::level_win_scene(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_comesinthenight.play(_volume);
+    }else if(rand_num == 1){
+        bn::sound_items::sb_bestgame.play(_volume);
+    }
+}
+
+void sb_commentary::stomp_peasant(){
+    unsigned short rand_num = rand() % 20;
+    if(rand_num == 0){
+        bn::sound_items::sb_asquisha.play(_volume);
+    }else if(rand_num == 1){
+        bn::sound_items::sb_asquishasquisha.play(_volume);
+    }else if(rand_num == 2){
+        bn::sound_items::sb_squishedyou.play(_volume);
+    }
+}
+
+void sb_commentary::ignite_peasant(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_hesonfire.play(_volume);
+    }else if(rand_num == 1){
+        bn::sound_items::sb_lookit_him_burning.play(_volume);
+    }
+}
+
+void sb_commentary::ignite_cottage(){
+    unsigned short rand_num = rand() % 4;
+    if(rand_num == 0){
+        bn::sound_items::sb_dooj.play(_volume);
+    }
+}
+
 }
 
