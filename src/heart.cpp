5693e3332de10cc31152a146df078ce38a9bdf16
Jeremy Elkayam <jeremyelkayam@gmail.com>
2022-12-30 17:51:51 -0500

    heart class

---
+++ src/heart.cpp
@@ -0,0 +1,33 @@
+#include <bn_log.h>
+#include "heart.h"
+
+namespace trog { 
+
+heart::heart(int xcor, int ycor) : 
+        entity(xcor, ycor, 0, 0, bn::sprite_items::heart.create_sprite(xcor, ycor)) {
+    
+}
+
+void heart::update(){
+    entity::update();
+}
+void heart::update_anim(){
+    entity::update_anim();
+
+    if(_anim){
+        if(!_anim->done()){
+            _anim->update();
+        }else{
+            _sprite.set_visible(false);
+        }
+    }
+}
+void heart::dissipate(){
+    _anim = bn::create_sprite_animate_action_forever(
+                _sprite, 5, bn::sprite_items::heart.tiles_item(),
+                0, 1, 2, 3 );
+}
+
+
+}
+
