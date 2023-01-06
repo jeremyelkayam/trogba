c158b07ad5b0d272878c3f7124c99bf30b203ea7
Jeremy Elkayam <jeremyelkayam@gmail.com>
2023-01-06 17:14:23 -0500

    added butano credit

--- src/credits_scene.cpp
+++ src/credits_scene.cpp
@@ -3,6 +3,7 @@
 #include <bn_sprite_text_generator.h>
 #include <bn_string.h>
 #include <bn_version.h>
+#include <bn_sound_items.h>
 #include "credits_scene.h"
 #include "bn_sprite_items_titlegraphic.h"
 #include "constants.h"
@@ -26,10 +27,18 @@ void credits_scene::setup_credits(){
     _text_generator.set_center_alignment();
     _text_generator.set_palette_item(WHITE_PALETTE);
 
+    bn::string<64> butano_str;
+    bn::ostringstream butano_string_stream(butano_str);
+    butano_string_stream << "Created using Butano " << bn::version::major() << "." << bn::version::minor();
+
     bn::string<64> credits[] = {
         "The GBA Game",
         "",
         "programmed by Jeremy Elkayam",
+        "",
+        butano_str,
+        "by Gustavo Valiente",
+        "github.com/GValiente/butano",
         "",
         "Thanks to",
         "Mips96 + TrogdorReburninated",
@@ -62,9 +71,10 @@ void credits_scene::setup_credits(){
         "Aeon Softworks",
         "",
         "Board game by ",
-        "The Brothers Chaps ",
+        "The Brothers Chaps",
         "and James Ernest"
     };
+    bn::sound_items::cutscene_credits.play(TROG_DEFAULT_VOLUME);
 
     for(bn::string<64> str : original_game_credits){ 
         _text_generator.generate(0, ycor,str, _text_sprites);
@@ -84,8 +94,7 @@ bn::optional<scene_type> credits_scene::update(){
     ++_time;
 
     if(bn::keypad::a_held()){
-        speed*=3;
-        _time+=2;
+        speed*=4;
     }
 
     for(bn::sprite_ptr &sprite : _title_sprites ){
@@ -96,7 +105,7 @@ bn::optional<scene_type> credits_scene::update(){
         sprite.set_y(sprite.y() - speed);
     }
 
-    if(_time > 1200){
+    if(_text_sprites.back().y() < -90){
         result = scene_type::TITLE;
     }
 
