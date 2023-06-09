1dadbfa3c8efa679c09dad09782a6d499bd41293
Jeremy Elkayam <jeremyelkayam@gmail.com>
2023-06-09 11:03:58 -0400

    shitty burninate anim

--- include/burninate_text.h
+++ include/burninate_text.h
@@ -3,14 +3,16 @@
 
 #pragma once
 #include <bn_sprite_actions.h>
+#include <bn_sprite_animate_actions.h>
 #include "big_text.h"
 
 namespace trog {
     class burninate_text : public big_text { 
         private:
-            // bn::sprite_ptr _fire;
-            // bn::optional<bn::sprite_scale_loop_action> _fire_scale;
-            // bn::sprite_move_loop_action _fire_move;
+            bn::sprite_ptr _fire;
+            bn::optional<bn::sprite_scale_loop_action> _fire_scale;
+            bn::sprite_move_loop_action _fire_move;
+            bn::sprite_animate_action<4> _fire_anim;
 
         public:
             burninate_text(bn::sprite_text_generator &generator);
