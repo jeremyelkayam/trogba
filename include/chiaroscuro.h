5d880a488d1b2ff1ed4d7429906057913fc59979
Jeremy Elkayam <jeremyelkayam@gmail.com>
2025-04-07 08:53:39 -0400

    chiaroscuro idle

--- include/chiaroscuro.h
+++ include/chiaroscuro.h
@@ -6,7 +6,7 @@ namespace trog {
     class chiaroscuro : public player { 
         private:
             // bn::optional<bn::sprite_animate_action<7>> _flex;
-            bn::sprite_animate_action<4> _walkcycle;
+            bn::sprite_animate_action<6> _walkcycle;
 
         public: 
             chiaroscuro(bn::fixed xcor, bn::fixed ycor, session_info &sesh, bool iframes, common_stuff &common_stuff, uint8_t initial_trogmeter=0);
